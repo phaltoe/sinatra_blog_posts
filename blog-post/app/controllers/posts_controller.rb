e34aab02673c0d8f265224cbc9287efb498d8ff9
phaltoe <reverse9@live.com>
2016-06-06 19:10:06 -0300

    Fixed Styling

--- blog-post/app/controllers/posts_controller.rb
+++ blog-post/app/controllers/posts_controller.rb
@@ -1,36 +1,36 @@
 class PostsController < ApplicationController
 
-	get '/posts' do
-		@posts = Post.all
-		erb :'/posts/index'	
-	end
+  get '/posts' do
+    @posts = Post.all
+    erb :'/posts/index'
+  end
 
-	get '/posts/new' do
-		erb :'/posts/new'
-	end
+  get '/posts/new' do
+    erb :'/posts/new'
+  end
 
-	post '/posts' do 
-		@post = Post.new(params[:post])
-		@post.author = Author.find_or_create_by(:name => params[:author_name])
-		@post.save
+  post '/posts' do
+    @post = Post.new(params[:post])
+    @post.author = Author.find_or_create_by(:name => params[:author_name])
+    @post.save
 
-		redirect ("/posts/#{@post.id}")
-	end
+    redirect ("/posts/#{@post.id}")
+  end
 
-	get '/posts/:id' do
-		@post = Post.find(params[:id])
-		erb :'/posts/show'
-	end
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'/posts/show'
+  end
 
-	get '/posts/:id/edit' do
-		@post = Post.find(params[:id])
-		erb :'/posts/edit'
-	end
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :'/posts/edit'
+  end
 
-	patch '/posts/:id' do
-		@post = Post.find(params[:id])
-		@post.author = Author.find_or_create_by(:name => params[:author_name])
-		@post.update(params[:post])
-		redirect ("posts/#{@post.id}")
-	end
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.author = Author.find_or_create_by(:name => params[:author_name])
+    @post.update(params[:post])
+    redirect ("posts/#{@post.id}")
+  end
 end
