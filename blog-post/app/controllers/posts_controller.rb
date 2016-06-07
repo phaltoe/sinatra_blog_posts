c66bc1e3430a6559b6dac17fa66ec9e517f25928
phaltoe <reverse9@live.com>
2016-06-07 12:09:53 -0300

    post_controller styling

--- blog-post/app/controllers/posts_controller.rb
+++ blog-post/app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   get '/posts' do
     @posts = Post.all
+    
     erb :'/posts/index'
   end
 
@@ -20,18 +21,20 @@ class PostsController < ApplicationController
 
   get '/posts/:id' do
     @post = Post.find(params[:id])
+
     erb :'/posts/show'
   end
 
   get '/posts/:id/edit' do
     @post = Post.find(params[:id])
+
     erb :'/posts/edit'
   end
 
   patch '/posts/:id' do
     @post = Post.find(params[:id])
-    # @post.author = Author.find_or_create_by(:name => params[:author_name])
     @post.update(params[:post])
+
     redirect ("posts/#{@post.id}")
   end
 end
